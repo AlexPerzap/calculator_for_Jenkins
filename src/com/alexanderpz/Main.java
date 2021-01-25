a318f57d688706078f2e132fca154dfefb09ad12
Alexander Perez Zapata <alexander.pz@globant.com>
2021-01-24 21:23:47 -0500

    sum and sub

--- src/com/alexanderpz/Main.java
+++ src/com/alexanderpz/Main.java
@@ -3,6 +3,14 @@ package com.alexanderpz;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(sum(3,6));
+        System.out.println(subtract(30,6));
+    }
+
+    public static double sum(double num1, double num2) {
+        return num1 + num2;
+    }
+    public static double subtract(double num1, double num2) {
+        return num1 - num2;
     }
 }
