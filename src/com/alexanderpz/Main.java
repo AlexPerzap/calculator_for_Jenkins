f85ad2363359842fe56ce7905299803d455f126c
Alexander Perez Zapata <alexander.pz@globant.com>
2021-01-24 21:29:39 -0500

    conflicts resolved

--- src/com/alexanderpz/Main.java
+++ src/com/alexanderpz/Main.java
@@ -3,8 +3,11 @@ package com.alexanderpz;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println(sum(3,6));
         System.out.println(subtract(30,6));
+        System.out.println(multiply(3,6));
+        System.out.println(divide(30,6));
     }
 
     public static double sum(double num1, double num2) {
@@ -12,5 +15,13 @@ public class Main {
     }
     public static double subtract(double num1, double num2) {
         return num1 - num2;
+    }
+
+    public static double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+    public static double divide(double num1, double num2) {
+        return num1 / num2;
+
     }
 }
